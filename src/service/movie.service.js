d3b6e8c238c5a5774b975f2e5c97231954c8fc41
Austin Reilly <areilly@alphaworks.tech>
2021-07-12 20:39:35 -0700

    working movie.service

--- src/service/movie.service.js
+++ src/service/movie.service.js
@@ -1,3 +1,17 @@
+// export default class MovieService
+// {
+// 	async getMovies(title)
+// 	{
+// 		const apiKey = '7ea59a4d';
+// 		const response = await fetch(
+// 			`https://www.omdbapi.com/?s=${title}&apikey=${apiKey}`, {
+// 				method: "GET"
+// 			}
+// 		);
+
+// 		return response.json();
+// 	}
+// }
 export default class MovieService
 {
 	async getMovies(title)
